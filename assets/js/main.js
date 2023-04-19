f0938fa6cfaba3b18784e30188bf736b77cc39eb
Romina Zapata <106558127+RomiErased@users.noreply.github.com>
2023-04-18 22:56:04 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 fetch("https://digimon-api.vercel.app/api/digimon")
     .then(response => response.json())
     .then(data => {
-        for (const post of data) { //item capturamos cada una de las cosas que tiene data
+        for (const post of data) { // capturamos cada una de las cosas que tiene data
             $("#table-posts tbody").append(`
                 <tr>
                     <td>${post.name}</td>
@@ -18,8 +18,8 @@ fetch("https://digimon-api.vercel.app/api/digimon")
 const searchButton = document.getElementById('btn-buscar');
 const searchField = document.getElementById('searchField');
 const resultsList = document.getElementById('resultsList');
-const navList = document.getElementById('nav-list');
 
+//función para buscar el digimon por nombre
 searchButton.addEventListener('click', (e) => {
     e.preventDefault()
     const query = searchField.value;
@@ -40,19 +40,24 @@ searchButton.addEventListener('click', (e) => {
         .catch(error => console.error(error));
 });
 
+//función para que muestre los digimon por nivel
+function verNivel(nivel) {
+    $("#digimon").addClass("d-none") //para que no oculte
+    $("#tabla").removeClass("d-none")
+    fetch(`https://digimon-api.vercel.app/api/digimon/level/${nivel}`)
+        .then(response => response.json())
+        .then(data => {
+            $("#table-posts tbody").html('');
+            data.forEach(post => {
+                $("#table-posts tbody").append(`
+                <tr>
+                    <td>${post.name}</td>
+                    <td> <img src="${post.img}"/></td> 
+                    <td>${post.level}</td>
+                </tr>
+            `)
+            });
+        })
+        .catch(error => console.error(error));
+}
 
-const nav = navList.value;
-fetch(`https://digimon-api.vercel.app/api/digimon/level/${nav}`)
-    .then(response => response.json())
-    .then(data => {
-        // Iterar sobre los datos y crear elementos del listado
-        data.forEach(item => {
-            const listItem = document.createElement('li');
-            const link = document.createElement('a');
-            link.textContent = item.nombre;
-            link.href = item.url;
-            listItem.appendChild(link);
-            navList.appendChild(listItem);
-        });
-    })
-    .catch(error => console.log(error));
